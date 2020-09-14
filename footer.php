ea1a210655732070f3a884af15cf2fd40b3bd9d4
Travis Nice <travis@nice.id.au>
2020-09-14 18:45:46 +1000

    New Footer
    Updated footer alignment, styling, sizing, and border.

--- footer.php
+++ footer.php
@@ -13,17 +13,21 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nicepress' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'nicepress' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'nicepress' ), 'NicePress', '<a href="https://www.nice.id.au/">Travis Nice</a>' );
-				?>
+			<small>
+				<em>
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nicepress' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'nicepress' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep">&nbsp;|&nbsp;</span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'nicepress' ), 'NicePress', '<a href="https://www.nice.id.au/">Travis Nice</a>' );
+						?>
+				</em>
+			</small>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
